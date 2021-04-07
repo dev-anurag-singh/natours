15ca37fea45aac406da309bfce787832e9997243
Anurag <dev.anuragkr@gmail.com>
2021-04-08 00:41:01 +0530

    updated stripe implementation

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingsModel');
 const catchAsync = require('../util/catchAsync');
 const AppError = require('./../util/appError');
 
@@ -26,10 +27,16 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   const tourIDs = bookings.map((el) => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-  res.status(200).render('overview', {
-    title: 'My Tours',
-    tours,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com * ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('overview', {
+      title: 'My Tours',
+      tours,
+    });
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
